0b99395ed019b27e76353156dc7de63e7ef32f7e
Oleksandr Tischik <alex.tischik@i.ua>
2023-10-18 01:17:17 +0300

    module 3 (10 done)

--- hr_hospital/models/hospital_patient_visit.py
+++ hr_hospital/models/hospital_patient_visit.py
@@ -13,11 +13,15 @@ class HospitalPatientVisit(models.Model):
     patient_id = fields.Many2one(comodel_name='hospital.patient')
     date = fields.Datetime(readonly=False,
                            states={'done': [('readonly', True)]})
+    date_only = fields.Date(compute='_compute_date_only', readonly=True)
     doctor_id = fields.Many2one(comodel_name='hospital.doctor', readonly=False,
                                 states={'done': [('readonly', True)]})
-    diagnosis_ids = fields.Many2many(comodel_name='hospital.diagnosis')
-    schedule_sel = fields.Selection(selection='_selection_schedule')
-    schedule = fields.Many2one(comodel_name='hospital.doctor.schedule')
+    diagnosis_ids = fields.Many2many(comodel_name='hospital.diagnosis',
+                                     domain="[('doctor_id', '=', doctor_id), "
+                                            "('patient_id', '=', patient_id)]")
+    schedule = fields.Many2one(comodel_name='hospital.doctor.schedule',
+                               domain="[('doctor_id', '=', doctor_id), "
+                                      "('date', '=', date_only)]")
 
     @api.model
     def cron_done(self):
@@ -29,21 +33,11 @@ class HospitalPatientVisit(models.Model):
         #    print('rec.date = ', rec.date)
         found_sp.write({'state': 'done'})
 
-    def _selection_schedule(self):
-        res_find = self.schedule.search([('doctor_id.id', '=',
-                                          self.doctor_id.id),
-                                        # ('date', '=', self.date.Date())
-                                         ])
-        res = []
-        for fin_rec in res_find:
-            res.append((fin_rec.id, fin_rec.name))
-        return res
-
     @api.onchange('schedule')
     def _onchange_schedule(self):
         for rec in self:
-            find_count = self.search_count([('schedule', '=', rec.schedule),
-                                            ('id', '!=', rec.id)])
+            find_count = self.search_count([('schedule.id',
+                                             '=', rec.schedule.id)])
             if find_count > 0:
                 raise ValidationError(_('This time is already taken'))
 
@@ -51,24 +45,21 @@ class HospitalPatientVisit(models.Model):
     def _set_done(self):
         self.cron_done()
 
+    @api.depends('date')
+    def _compute_date_only(self):
+        for rec in self:
+            rec.date_only = fields.Date.to_date(rec.date)
+
     @api.constrains('active')
     def _constrains_date(self):
         for rec in self:
-            if len(rec.disease_ids) > 0 and (not rec.active):
+            if len(rec.diagnosis_ids) > 0 and (not rec.active):
                 raise ValidationError(_('You cannot archive '
                                         'a visit with a diagnosis'))
 
     @api.ondelete(at_uninstall=False)
     def _ondelete_patient_visit(self):
         for rec in self:
-            if len(rec.disease_ids) > 0:
+            if len(rec.diagnosis_ids) > 0:
                 raise ValidationError(_('You cannot delete '
                                         'a visit with a diagnosis'))
-
-    @api.constrains('diagnosis_ids')
-    def _constrains_mentor(self):
-        for rec in self:
-            for diag in self.diagnosis_ids:
-                if rec.doctor_id != diag.doctor_id:
-                    raise ValidationError(_('Intern ' + rec.name +
-                                            ' cannot be a mentor'))
--- hr_hospital/models/hospital_person.py
+++ hr_hospital/models/hospital_person.py
@@ -7,6 +7,7 @@ class HospitalPerson(models.AbstractModel):
 
     name = fields.Char(string='Full name', required=True)
     phone = fields.Char()
+    email = fields.Char(string='e-mail')
     photo = fields.Image(max_width=1980, max_height=1980, store=True)
     gender = fields.Selection(default='male',
                               selection=[('male', 'Male'),
